b718d71f666a8cb833a3a00bd25fdb8974f708fb
Taylor F Stiger <taylorfgomez@gmail.com>
2019-04-23 20:35:22 -0500

    finished scrape function

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -9,9 +9,9 @@ var scrape = function (cb) {
         var $ = cheerio.load(body);
 
         var articles = [];
-
+        // grabs article class "theme-summary"
         $(".theme-summary").each(function (i, element) {
-
+            // selects the heading and summary of the article which are children of that class
             var head = $(this).children(".story-heading").text().trim();
             var sum = $(this).children('.summary').text().trim();
 
@@ -20,14 +20,17 @@ var scrape = function (cb) {
                 var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, "" ).trim();
                 var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, "" ).trim();
                 
+                // makes object (dataTo Add) out of headNeat & sumNeat and assigns to attributes of headline and summary that required to create an article in our model
                 var dataToAdd = {
                     headline: headNeat,
                     summary: sumNeat
                 };
 
+                // push the new dataToAdd into our articles array and continues to iterate
                 articles.push(dataToAdd);
             }
         });
+        // callback sends the articles array
         cb(articles);
     });
 };
